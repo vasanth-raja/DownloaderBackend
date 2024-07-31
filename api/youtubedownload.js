802bfe9bbc7ee43c53d9ca007f5a03539c9ff366
vasanth-raja <vasanthrajark@gmail.com>
2024-07-31 10:47:26 +0530

    youtube fix

--- api/youtubedownload.js
+++ api/youtubedownload.js
@@ -2,6 +2,8 @@ const express= require('express')
 const router=express.Router()
 const axios=require('axios')
 const ytdl=require('ytdl-core')
+const { Innertube, UniversalCache, Utils } = require('youtubei.js');
+const { existsSync, mkdirSync} = require('fs');
 router.get('/',(req,res,next)=>{
     console.log("GET")
     res.status(200).json({
@@ -17,7 +19,7 @@ router.post('/',async(req,res,next)=>{
             if (info.formats) {
                 // const downloadLink = response.data.link; // Adjust based on actual API response structure.
             //    const data=info.formats;
-            const data = ytdl.filterFormats(info.formats, format => format.hasVideo && format.hasAudio)
+            const data = ytdl.filterFormats(info.formats, format => format.hasVideo && format.container=="mp4")
             const details=info.videoDetails;
             //    console.log(data.map((format)=>format.url))
             res.status(200).json({ message: 'Download links',data: data,details: details });
@@ -32,32 +34,44 @@ router.post('/',async(req,res,next)=>{
         }
     }
     const downloadLink = await ytVideoDownload();
-    console.log(downloadLink)
 })
-router.post('/download',async(req,res,next)=>{
+router.get('/download',async(req,res,next)=>{
         try {
-            const videoUrl = req.body.url;
-            const quality=req.body.quality;
-            console.log(req.body)
-            
-            if (!videoUrl) {
+            const url = req.query.link;
+              const videoId = await ytdl.getURLVideoID(url);
+            if (!url) {
                 return res.status(400).send('No URL provided');
               }
             
-              res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-              console.log("....................");
-              ytdl(videoUrl, { filter: format => format.container === 'mp4' && format.hasAudio && format.hasVideo && format.qualityLabel==quality})
-                .pipe(res)
-                .on('finish', () => {
-                    console.log(".....finish......");
-                   
-                  console.log('Video downloaded and sent to client successfully!');
-                })
-                .on('error', (err) => {
-                    console.log(".....err......");
-                  console.error('Error downloading video:', err);
-                  res.status(500).send('Error downloading video');
-                });
+              
+             
+              console.log(videoId);
+              itags=[299,298,135,134,133,160]
+              audioItag=[140]
+              const yt = await Innertube.create({ cache: new UniversalCache(false), generate_session_locally: true });
+              const metaInfo = await yt.getBasicInfo(videoId);
+              console.log(metaInfo.streaming_data.adaptive_formats.filter((item) =>item.has_audio ));
+              // itags.includes(item.itag)
+              const stream = await yt.download(videoId, {
+                  type: 'video+audio', // audio, video or video+audio
+                  quality: 'best', // best, bestefficiency, 144p, 240p, 480p, 720p and so on.
+                  format: 'mp4' // media container format 
+              });
+      
+              console.log("stream",stream);
+      
+              res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
+              res.setHeader('Content-Type', 'video/mp4');
+      
+              // const file = createWriteStream(`${dir}/downloaded_video.mp4`);
+      
+              for await (const chunk of Utils.streamToIterable(stream)) {
+                  // file.write(chunk);
+                  res.write(chunk);
+
+              }
+              
+              res.end();
             
             // res.json({ downloadLink });
         } catch (error) {
