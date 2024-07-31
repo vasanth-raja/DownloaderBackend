c64450703875554c0f9844e3ea462269cdce3995
vasanth-raja <vasanthrajark@gmail.com>
2024-07-31 15:44:44 +0530

    youtube fix 3

--- api/youtubedownload.js
+++ api/youtubedownload.js
@@ -1,44 +1,50 @@
 const express= require('express')
 const router=express.Router()
 const axios=require('axios')
-const ytdl=require('ytdl-core')
 const { Innertube, UniversalCache, Utils } = require('youtubei.js');
-const { existsSync, mkdirSync} = require('fs');
+
 router.get('/',(req,res,next)=>{
     console.log("GET")
     res.status(200).json({
         message: 'Get request recieved'
     })
 })
+function extractVideoId(url) {
+      const regex = /(?:https?:\/\/)?(?:www\.)?youtube\.com\/(?:watch\?v=|embed\/|v\/|.*\/)?([a-zA-Z0-9_-]{11})|(?:https?:\/\/)?youtu\.be\/([a-zA-Z0-9_-]{11})/;
+      const match = url.match(regex);
+      return match ? match[1] || match[2] : null;
+    }
 router.post('/',async(req,res,next)=>{
-
-    async function ytVideoDownload() {
-        try {
-            const info = await ytdl.getInfo(req.body.url);
-            console.log(info)
-            if (info.formats) {
-                // const downloadLink = response.data.link; // Adjust based on actual API response structure.
-            //    const data=info.formats;
-            const data = ytdl.filterFormats(info.formats, format => format.hasVideo && format.container=="mp4")
-            const details=info.videoDetails;
-            //    console.log(data.map((format)=>format.url))
-            res.status(200).json({ message: 'Download links',data: data,details: details });
-            } else {
-                console.log("error")
-                res.status(400).json({ message: 'Failed to get download link' });
-            }
-            // res.json({ downloadLink });
+    try {
+        const url = req.body.url;
+          const videoId =extractVideoId(url);
+        if (!url) {
+            return res.status(400).send('No URL provided');
+          }
+        
+          
+         
+          console.log(videoId);
+          itags=[299,298,135,134,133,160]
+          audioItag=[140]
+          const yt = await Innertube.create({ cache: new UniversalCache(false), generate_session_locally: true });
+          const metaInfo = await yt.getBasicInfo(videoId);
+          const videoDetails=metaInfo.basic_info
+          const videoFormats=metaInfo.streaming_data.adaptive_formats.filter((item) =>itags.includes(item.itag) )
+          console.log(metaInfo)
+          console.log("Video Formats",videoFormats);
+          res.status(200).json({videoDetails:videoDetails,videoFormats:videoFormats})
         } catch (error) {
+            console.log(".....finish......");
             console.error('Error fetching download link:', error);
             res.status(400).json({ message: 'Failed to get download link' });
         }
-    }
-    const downloadLink = await ytVideoDownload();
+    
 })
 router.get('/download',async(req,res,next)=>{
         try {
             const url = req.query.link;
-              const videoId = await ytdl.getURLVideoID(url);
+              const videoId =extractVideoId(url);
             if (!url) {
                 return res.status(400).send('No URL provided');
               }
